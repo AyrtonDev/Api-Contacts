007dbcf863d4691a7df00e92e6f46af64d866c05
Ayrton <ayrtong.guerra@gmail.com>
2023-11-05 00:30:31 -0400

    feat: create update and delete controller and repository

--- app/controllers/ContactController.py
+++ app/controllers/ContactController.py
@@ -29,6 +29,13 @@ class ContactController:
             return build_reponse(
                 message="Contact not found",
                 data=None,
+                status=500
+            )
+
+        if contact == "Contact not found":
+            return build_reponse(
+                message="Contact not found",
+                data=None,
                 status=404
             )
 
@@ -57,18 +64,97 @@ class ContactController:
 
         newContact = self.contact_repository.add(contact)
 
+        if newContact is None:
+            return build_reponse(
+                message="Internal error",
+                data=None,
+                status=500
+            )
+
         return build_reponse(
             message="Contact added",
             data={'id': newContact},
             status=201
         )
 
-    def update_contact(self, contact_id, contact):
-        return self.contact_repository.update_contact(contact_id, contact)
+    def update_contact(self, contact_id):
+        contact = request.get_json()
+
+        is_uuid = is_valid_uuid(contact_id)
+
+        if is_uuid is False:
+            return build_reponse(
+                message="Invalid contact id",
+                data=None,
+                status=400
+            )
+
+        if verify_attrb(contact, 'name'):
+            return build_reponse(
+                message="Name is required",
+                data=None,
+                status=400
+            )
+
+        if verify_attrb(contact, 'email') or \
+            verify_attrb(contact, 'phone'):
+            return build_reponse(
+                message="E-mail or phone field have to be full",
+                data=None,
+                status=400
+            )
+
+        contact_updated = self.contact_repository.update_by_id(contact_id, contact)
+
+        if contact_updated is None:
+            return build_reponse(
+                message="Internal error",
+                data=None,
+                status=500
+            )
+
+        if contact_updated == "Contact not found":
+            return build_reponse(
+                message="Contact not found",
+                data=None,
+                status=404
+            )
+
+        return build_reponse(
+                message="Contact was updated",
+                data={'id': contact_id }
+            )
 
     def delete_contact(self, contact_id):
-        return self.contact_repository.delete_contact(contact_id)
+        is_uuid = is_valid_uuid(contact_id)
+
+        if is_uuid is False:
+            return build_reponse(
+                message="Invalid contact id",
+                data=None,
+                status=400
+            )
+
+        contact = self.contact_repository.delete_by_id(contact_id)
+
+        if contact is None:
+            return build_reponse(
+                message="Internal error",
+                data=None,
+                status=500
+            )
+
+        if contact == "Contact not found":
+            return build_reponse(
+                message="Contact not found",
+                data=None,
+                status=404
+            )
+
+        return build_reponse(
+            message=f"Contact {contact_id} was deleted",
+            data=None,
+            status=204
+        )
 
 contact_controller = ContactController(ContactRepository())
-
-# TODO: finish all controllers
--- app/repositories/ContactRepository.py
+++ app/repositories/ContactRepository.py
@@ -1,22 +1,25 @@
 from app import db
 from app.models.Contacts import Contacts
-from app.models.Contacts import ContactProps
 
 class ContactRepository:
     def add(self, contact):
-        new_contact = Contacts(
-            name=contact['name'],
-            email=contact['email'],
-            phone=contact['phone'],
-            category_id=contact['categoryId']
-        )
+        try:
+            new_contact = Contacts(
+                name=contact['name'],
+                email=contact['email'],
+                phone=contact['phone'],
+                category_id=contact['category_id']
+            )
 
-        db.session.add(new_contact)
-        db.session.commit()
+            db.session.add(new_contact)
+            db.session.commit()
 
-        return new_contact.id
+            return new_contact.id
 
-    def get_all(self) -> list[ContactProps] | list :
+        except:
+            return None
+
+    def get_all(self):
         contacts_list = []
         contacts = Contacts.query.all()
 
@@ -31,24 +34,60 @@ class ContactRepository:
 
         return contacts_list
 
-    def get_by_id(self, contact_id) -> ContactProps | None:
-        contact = Contacts.query.filter_by(id=contact_id).first()
+    def get_by_id(self, contact_id):
+        try:
+            contact = Contacts.query.filter_by(id=contact_id).first()
 
-        if contact:
+            if contact is None:
+                return "Contact not found"
+
             return {
-                'id': contact.id,
-                'name': contact.name,
-                'email': contact.email,
-                'phone': contact.phone,
-                'category_id': contact.category_id
-            }
-        else:
+                    'id': contact.id,
+                    'nome': contact.name,
+                    'phone': contact.phone,
+                    'email': contact.email,
+                    'category_id': contact.category_id
+                }
+
+        except:
             return None
 
     def delete_by_id(self, contact_id):
-        return contact_id
+        try:
+            contact = Contacts.query.filter_by(id=contact_id).first()
+
+            if contact is None:
+                return "Contact not found"
+
+            db.session.delete(contact)
+            db.session.commit()
+
+            return contact_id
+
+        except:
+            return None
 
     def update_by_id(self, contact_id, contact_data):
-        return contact_id
+        try:
+            contact = Contacts.query.filter_by(id=contact_id).first()
 
-# TODO: finish all repositories
+            if contact is None:
+                return "Contact not found"
+
+            contact.name = contact_data['name']
+
+            if 'email' in contact_data:
+                contact.email = contact_data['email']
+
+            if 'phone' in contact_data:
+                contact.phone = contact_data['phone']
+
+            if 'category_id' in contact_data:
+                contact.category_id = contact_data['category_id']
+
+            db.session.commit()
+
+            return contact_id
+
+        except:
+            return None
