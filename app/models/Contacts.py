2a0aef0728e79fccc3107351241424db9aafe53e
Ayrton <ayrtong.guerra@gmail.com>
2023-11-05 05:09:50 -0400

    chore: remove class useless

--- app/models/Contacts.py
+++ app/models/Contacts.py
@@ -1,16 +1,6 @@
 import uuid
 from app import db
 from sqlalchemy.dialects.postgresql import UUID
-from typing import Optional
-
-class ContactProps:
-    def __init__(self, id: Optional[str | uuid.UUID], name: str, phone: str, email:str, category_id:str | uuid.UUID):
-        self.id = id
-        self.name = name
-        self.phone = phone
-        self.email = email
-        self.category_id = category_id
-
 
 class Contacts(db.Model):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, unique=True, nullable=False)
